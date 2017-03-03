8262554f85189642e3202b9bc4ee637d3eb8b48b
yura.lee <2youlol@gmail.com>
2017-03-03 12:17:52 +0900

    upload

---
+++ src/js/ui.js
@@ -0,0 +1,20 @@
+$(function(){
+		$('.l_visual > li > a').fullScreen();
+
+
+
+	$('.visual').slide({
+		slide_type : 'mono',
+		setAuto : true,
+		auto_speed : 4000
+	});
+	$('.notice_wrap').slide({
+		slide_type : 'mono',
+		direction : 'vertical',
+		setAuto : true,
+		auto_speed : 2000
+	});
+
+
+
+})
